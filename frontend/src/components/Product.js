3e30efccdfcbf632d6e520d58eb6377bb7921036
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-26 09:33:24 +0500

    Home Screen and product Listings

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function Product({ product }) {
+  return (
+    <Card className='p-3 my-3 rounded'>
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as='div'>
+          <div className='my-3'>
+            {product.rating} from {product.numReviews}
+          </div>
+        </Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
