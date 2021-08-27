9ada8379b2953d9de06c1369ead1db75466a78af
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-27 09:00:57 +0500

    Products Details Actions and Reducers

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./reducers/productReducers";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const middlewares = [thunk];
