e0a908e1efe7a542e6d24781295b11fd05426869
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-29 16:30:41 +0500

    get Order Details routes

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -32,4 +32,17 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("No order found with this id");
+  }
+});
+
+export { addOrderItems, getOrderById };
