d8000d9965385e6337e4ab08c74025363c4df64d
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-08-31 04:06:57 +0500

    Image Upload Config and endpoint

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,41 @@
+import express from "express";
+import path from "path";
+import multer from "multer";
+const router = express();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const fileType = /jpg|png|jpeg/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("Images only supported");
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post("/profile", upload.single("image"), (req, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productroutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddlewares.js";
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use("/api/products", productroutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
